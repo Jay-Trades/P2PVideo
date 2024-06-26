0d1d64eb14e267496ab27e2efb8d4e98c7b55e19
Jay <jaycc@seas.upenn.edu>
2024-06-26 11:37:17 -0400

    set up working connection to the db and also generator seperate db connection for the route because fastapi is async

--- server/login.py
+++ server/login.py
@@ -1,19 +1,24 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-import sqlite3
+from database import get_db
+from sqlite3 import Connection
+from typing import Generator
 
-conn = sqlite3.connect('login.db')
-cursor = conn.cursor()
 
 
 router = APIRouter()
 
+def get_db_conn() -> Generator:
+    with get_db() as db:
+        yield db
+
 @router.post("/registration")
-def login(email: str, username: str, password: str):
+def login(email: str, username: str, password: str, db: Connection = Depends(get_db_conn)):
     # Implement your login logic here
+    cursor = db.cursor()
+    cursor.execute('SELECT * FROM User')
+    rows = cursor.fetchall()
+    print(rows)
     if username == "test" and password == "password":
-        cursor.execute('SELECT * FROM users')
-        rows = cursor.fetchall()
-        print(rows)
         return {"message": "Login successful"}
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
 
